bdac4c9697a0faa0f1bf6748d22298f805268e73
Craig McKeachie <craigdmckeachie@gmail.com>
2024-05-31 11:37:48 -0400

    refactor into functions

--- js/alpha-addition.js
+++ js/alpha-addition.js
@@ -2,18 +2,28 @@
 
 function addLetters(...letters) {
   if (letters.length === 0) return "z";
-  //convert to array
-  const lettersArray = letters;
-  //convert letters to numbers
-  const numbers = lettersArray.map((l) => l.charCodeAt(0) - 96);
-  //sum numbers
-  let total = numbers.reduce((previous, current) => previous + current);
+  
+  const numbers = convertLettersToNumbers(letters);
+
+  let total = sum(numbers);
   if (total > 26) {
     total = total % 26;
   }
-  console.log(total);
-  //convert total to number
-  return String.fromCharCode(96 + total);
+  // console.log(total);
+
+  return covertNumberToLetter(total);
+}
+
+function sum(numbers) {
+  return numbers.reduce((previous, current) => previous + current);
+}
+
+function convertLettersToNumbers(letters) {
+  return letters.map((l) => l.charCodeAt(0) - 96);
+}
+
+function covertNumberToLetter(number) {
+  return String.fromCharCode(96 + number);
 }
 
 const result = addLetters("a", "b", "c");
